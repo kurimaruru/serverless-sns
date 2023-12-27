9bb90bb5f23511004a83d77967d1e6ff5e150429
[栗原陸] <[riku.kurihara@genialtech.io]>
2023-12-27 12:19:48 +0900

    feat: 認証機能の実装
     - next-authを利用した認証の実装

--- frontend/src/app/components/elements/header.tsx
+++ frontend/src/app/components/elements/header.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { Logout, PersonAdd, Settings } from "@mui/icons-material";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { Divider, ListItemIcon, Menu, MenuItem } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
@@ -7,15 +10,31 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import { signOut } from "next-auth/react";
+import { useState } from "react";
 
 export const Header = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const signOutHandler = () => {
+    console.log("ログアウトしました");
+    signOut({ callbackUrl: "/" });
+  };
+
   return (
     <AppBar component="nav">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <Tooltip title="Open settings">
-              <IconButton sx={{ p: 0 }}>
+              <IconButton onClick={handleClick} sx={{ p: 0 }}>
                 <Avatar
                   alt="Remy Sharp"
                   src="https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1-768x768.png"
@@ -23,6 +42,67 @@ export const Header = () => {
               </IconButton>
             </Tooltip>
           </Box>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&::before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Avatar /> Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <PersonAdd fontSize="small" />
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem onClick={signOutHandler}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
           <TwitterIcon />
           <Typography
             variant="h5"
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,48 @@
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
+import type { Session } from "next-auth";
+import { headers } from "next/headers";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const _Home = () => {
+async function fetchSession(cookie: string): Promise<Session | null> {
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
+    headers: {
+      cookie,
+    },
+  });
+  const session = (await response.json()) as Session;
+  return Object.keys(session).length > 0 ? session : null;
+}
+type ValidSession = {
+  user: {
+    name: string;
+    email: string;
+    image: string;
+  };
+  expires: string;
+  appAccessToken: string;
+};
+const isValidSession = (session: Session | null): session is ValidSession => {
+  if (session) {
+    // サンプルコードなので手抜きしているが実際はちゃんと型検査をする
+    if (
+      session.user?.name != null &&
+      session.user.email != null &&
+      session.user.image != null
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+const _Home = async () => {
+  const session = await fetchSession(headers().get("cookie") ?? "");
+
+  if (isValidSession(session)) {
+    redirect("/home");
+  }
+
   return (
     <Grid
       container
@@ -17,11 +57,16 @@ const _Home = () => {
           alignItems: "center",
         }}
       >
-        <Button variant="contained">
-          <Link href="/home" style={{ textDecoration: "none", color: "white" }}>
-            Login
-          </Link>
-        </Button>
+        <>
+          <Button variant="contained">
+            <Link
+              href="/api/auth/callback/google"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Login
+            </Link>
+          </Button>
+        </>
       </Grid>
     </Grid>
   );
